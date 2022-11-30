d85877c63d3e59e67f1a2ef2bf0243426356c969
Achilles Portarinos <achilles.portarinos@zuehlke.com>
2022-11-30 15:13:53 +0100

    add todos for filters

--- src/core/Wemogy.Infrastructure.Database.Core/Attributes/RepositoryReadFilterAttribute.cs
+++ src/core/Wemogy.Infrastructure.Database.Core/Attributes/RepositoryReadFilterAttribute.cs
@@ -2,6 +2,10 @@ using System;
 
 namespace Wemogy.Infrastructure.Database.Core.Attributes;
 
+
+/// <summary>
+///     CAUTION! TODO: Ensure that it gets properly implemented
+/// </summary>
 [AttributeUsage(
     AttributeTargets.Interface,
     AllowMultiple = true)]

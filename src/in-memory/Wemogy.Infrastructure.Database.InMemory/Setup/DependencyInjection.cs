6c5587d2650b2efb2710f741275bbd595b000499
Kane Vo <kane.vo@codeleap.de>
2026-07-01 16:49:07 +0700

    feat(in-memory): add InMemoryPollingOutboxEventSource and AddInMemoryOutboxProcessor DI extension

--- src/in-memory/Wemogy.Infrastructure.Database.InMemory/Setup/DependencyInjection.cs
+++ src/in-memory/Wemogy.Infrastructure.Database.InMemory/Setup/DependencyInjection.cs
@@ -1,6 +1,12 @@
+using System;
 using Microsoft.Extensions.DependencyInjection;
+using Wemogy.Infrastructure.Database.Core.Abstractions;
 using Wemogy.Infrastructure.Database.Core.Setup;
 using Wemogy.Infrastructure.Database.InMemory.Factories;
+using Wemogy.Infrastructure.Database.InMemory.Outbox;
+using Wemogy.Infrastructure.Database.Outbox.Abstractions;
+using Wemogy.Infrastructure.Database.Outbox.Entities;
+using Wemogy.Infrastructure.Database.Outbox.Setup;
 
 namespace Wemogy.Infrastructure.Database.InMemory.Setup
 {
@@ -11,6 +17,22 @@ namespace Wemogy.Infrastructure.Database.InMemory.Setup
             return new DatabaseSetupEnvironment(
                 serviceCollection,
                 new InMemoryDatabaseClientFactory());
+        }
+
+        public static DatabaseSetupEnvironment AddInMemoryOutboxProcessor<TOutboxEvent, TPayload, THandler>(
+            this DatabaseSetupEnvironment environment,
+            string containerName,
+            Action<OutboxProcessorOptions>? configure = null)
+            where TOutboxEvent : OutboxEventBase<TPayload>, IEntityBase
+            where THandler : class, IOutboxEventHandler<TPayload>
+            where TPayload : class
+        {
+            environment.Services.AddSingleton<IOutboxEventSource<TOutboxEvent, TPayload>>(sp =>
+                new InMemoryPollingOutboxEventSource<TOutboxEvent, TPayload>(
+                    sp.GetRequiredService<IServiceScopeFactory>(),
+                    sp.GetRequiredService<OutboxProcessorOptions>()));
+
+            return environment.AddOutboxProcessor<TOutboxEvent, TPayload, THandler>(containerName, configure);
         }
     }
 }
